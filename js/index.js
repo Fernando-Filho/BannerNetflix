f1cccfe04819555990154157b251bd79fbef712a
Fernando Filho <fernando.fs.filhorn@gmail.com>
2022-08-13 11:22:53 -0300

    introdução do código

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function info(){
+    const infoDesejada = document.getElementsByName('info')
+    let conteudoSinopse = document.getElementsByClassName('Sinopse')
+    let conteudoTrailers = document.getElementsByClassName('Trailers')
+    let conteudoMais = document.getElementsByClassName('Mais')
+    let conteudoDetalhes = document.getElementsByClassName('Detalhes')
+    
+    if (infoDesejada[0].checked) {
+        conteudoSinopse.tyle=display"block";
+    } else if {
+        
+    }
+}
