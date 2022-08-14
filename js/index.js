eff1ebc2ab16b5c06fcf6d51ecbd3dd8b6e75673
Fernando Filho <fernando.fs.filhorn@gmail.com>
2022-08-14 17:56:06 -0300

    alguns testes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
 function info(){
-    const infoDesejada = document.getElementsByName('info')
-    let conteudoSinopse = document.getElementsByClassName('Sinopse')
-    let conteudoTrailers = document.getElementsByClassName('Trailers')
-    let conteudoMais = document.getElementsByClassName('Mais')
-    let conteudoDetalhes = document.getElementsByClassName('Detalhes')
+
+
+    let infoDesejada = document.getElementsByName('inforadio')
+    console.log(infoDesejada)
+    // document.querySelectorAll('div.sinopse').style.display = 'none';
+    // document.querySelectorAll('div.Trailers').style.display = 'none';
+    // document.querySelectorAll('div.Mais').style.display = 'none';
+    // document.querySelectorAll('div.Detalhes').style.display = 'none';
+
+    // addEventListener(document.getElementsByName('info')).checked
+    // // let conteudoSinopse = document.getElementsByClassName('Sinopse')
+    // // let conteudoTrailers = document.getElementsByClassName('Trailers')
+    // // let conteudoMais = document.getElementsByClassName('Mais')
+    // // let conteudoDetalhes = document.getElementsByClassName('Detalhes')
     
     if (infoDesejada[0].checked) {
-        conteudoSinopse.tyle=display"block";
-    } else if {
-        
+        document.querySelectorAll('#info .info_dados .sinopse').style.display = 'block';
+        document.querySelectorAll('#info .info_dados .Trailers').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Mais').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Detalhes').style.display = 'none';
+    } else if (infoDesejada[1].checked){
+        document.querySelectorAll('#info .info_dados .sinopse').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Trailers').style.display = 'block';
+        document.querySelectorAll('#info .info_dados .Mais').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Detalhes').style.display = 'none';
+    } else if (infoDesejada[2].checked){
+        document.querySelectorAll('#info .info_dados .sinopse').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Trailers').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Mais').style.display = 'block';
+        document.querySelectorAll('#info .info_dados .Detalhes').style.display = 'none';
+    } else if (infoDesejada[3].checked){
+        document.querySelectorAll('#info .info_dados .sinopse').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Trailers').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Mais').style.display = 'none';
+        document.querySelectorAll('#info .info_dados .Detalhes').style.display = 'block';
     }
 }
